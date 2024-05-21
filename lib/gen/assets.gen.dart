007c80cd4ee29b4a806f15e2daa0a2967bb1ebed
Aldekenzo <akaireden@gmail.com>
2024-05-21 16:12:36 +0500

    updated yaml

--- lib/gen/assets.gen.dart
+++ lib/gen/assets.gen.dart
@@ -8,8 +8,9 @@
 // ignore_for_file: directives_ordering,unnecessary_import,implicit_dynamic_list_literal,deprecated_member_use
 
 import 'package:flutter/widgets.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:vector_graphics/vector_graphics.dart';
 
 class $AssetsFontsGen {
   const $AssetsFontsGen();
@@ -58,16 +59,28 @@ class $AssetsFontsGen {
 class $AssetsIconsGen {
   const $AssetsIconsGen();
 
+  /// Directory path: assets/icons/brand
   $AssetsIconsBrandGen get brand => const $AssetsIconsBrandGen();
+
+  /// Directory path: assets/icons/check_out
   $AssetsIconsCheckOutGen get checkOut => const $AssetsIconsCheckOutGen();
+
+  /// Directory path: assets/icons/location
   $AssetsIconsLocationGen get location => const $AssetsIconsLocationGen();
+
+  /// Directory path: assets/icons/point
   $AssetsIconsPointGen get point => const $AssetsIconsPointGen();
 
   /// File path: assets/icons/prod.png
   AssetGenImage get prod => const AssetGenImage('assets/icons/prod.png');
 
+  /// Directory path: assets/icons/regular
   $AssetsIconsRegularGen get regular => const $AssetsIconsRegularGen();
+
+  /// Directory path: assets/icons/solid
   $AssetsIconsSolidGen get solid => const $AssetsIconsSolidGen();
+
+  /// Directory path: assets/icons/toggle
   $AssetsIconsToggleGen get toggle => const $AssetsIconsToggleGen();
 
   /// File path: assets/icons/user.svg
@@ -454,9 +467,11 @@ class Assets {
 }
 
 class AssetGenImage {
-  const AssetGenImage(this._assetName);
+  const AssetGenImage(this._assetName, {this.size = null});
 
   final String _assetName;
+
+  final Size? size;
 
   Image image({
     Key? key,
@@ -528,9 +543,20 @@ class AssetGenImage {
 }
 
 class SvgGenImage {
-  const SvgGenImage(this._assetName);
+  const SvgGenImage(
+    this._assetName, {
+    this.size = null,
+  }) : _isVecFormat = false;
+
+  const SvgGenImage.vec(
+    this._assetName, {
+    this.size = null,
+  }) : _isVecFormat = true;
 
   final String _assetName;
+
+  final Size? size;
+  final bool _isVecFormat;
 
   SvgPicture svg({
     Key? key,
@@ -545,19 +571,21 @@ class SvgGenImage {
     WidgetBuilder? placeholderBuilder,
     String? semanticsLabel,
     bool excludeFromSemantics = false,
-    SvgTheme theme = const SvgTheme(),
+    SvgTheme? theme,
     ColorFilter? colorFilter,
     Clip clipBehavior = Clip.hardEdge,
     @deprecated Color? color,
     @deprecated BlendMode colorBlendMode = BlendMode.srcIn,
     @deprecated bool cacheColorFilter = false,
   }) {
-    return SvgPicture.asset(
-      _assetName,
+    return SvgPicture(
+      _isVecFormat
+          ? AssetBytesLoader(_assetName,
+              assetBundle: bundle, packageName: package)
+          : SvgAssetLoader(_assetName,
+              assetBundle: bundle, packageName: package),
       key: key,
       matchTextDirection: matchTextDirection,
-      bundle: bundle,
-      package: package,
       width: width,
       height: height,
       fit: fit,
@@ -567,9 +595,8 @@ class SvgGenImage {
       semanticsLabel: semanticsLabel,
       excludeFromSemantics: excludeFromSemantics,
       theme: theme,
-      colorFilter: colorFilter,
-      color: color,
-      colorBlendMode: colorBlendMode,
+      colorFilter: colorFilter ??
+          (color == null ? null : ColorFilter.mode(color, colorBlendMode)),
       clipBehavior: clipBehavior,
       cacheColorFilter: cacheColorFilter,
     );
--- lib/src/core/service/injectable/injectable_service.config.dart
+++ lib/src/core/service/injectable/injectable_service.config.dart
@@ -4,8 +4,7 @@
 // InjectableConfigGenerator
 // **************************************************************************
 
-// ignore_for_file: unnecessary_lambdas
-// ignore_for_file: lines_longer_than_80_chars
+// ignore_for_file: type=lint
 // coverage:ignore-file
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
@@ -13,25 +12,25 @@ import 'package:get_it/get_it.dart' as _i1;
 import 'package:injectable/injectable.dart' as _i2;
 
 import '../../../features/profile/data/datasources/remote/i_profile_remote.dart'
-    as _i5;
-import '../../../features/profile/data/datasources/remote/radionets_remote_impl.dart'
     as _i6;
-import '../../../features/profile/data/repositories/profile_repository_impl.dart'
-    as _i8;
-import '../../../features/profile/domain/repositories/i_profile_remote.dart'
+import '../../../features/profile/data/datasources/remote/radionets_remote_impl.dart'
     as _i7;
+import '../../../features/profile/data/repositories/profile_repository_impl.dart'
+    as _i9;
+import '../../../features/profile/domain/repositories/i_profile_remote.dart'
+    as _i8;
 import '../../../features/profile/domain/usecases/delete_account_use_case.dart'
     as _i12;
 import '../../../features/profile/domain/usecases/log_out_use_case.dart'
     as _i10;
 import '../../../features/profile/domain/usecases/update_user_info_use_case.dart'
     as _i11;
-import '../../api/client/rest/dio_client.dart' as _i4;
+import '../../api/client/rest/dio_client.dart' as _i5;
 import '../../utils/helpers/connectivity_helper.dart' as _i3;
-import '../../utils/helpers/isolate_manager.dart' as _i9;
+import '../../utils/helpers/isolate_manager.dart' as _i4;
 
 extension GetItInjectableX on _i1.GetIt {
-  // initializes the registration of main-scope dependencies inside of GetIt
+// initializes the registration of main-scope dependencies inside of GetIt
   Future<_i1.GetIt> init({
     String? environment,
     _i2.EnvironmentFilter? environmentFilter,
@@ -41,31 +40,31 @@ extension GetItInjectableX on _i1.GetIt {
       environment,
       environmentFilter,
     );
-    gh.singleton<_i3.ConnectivityHelper>(_i3.ConnectivityHelper());
-    await gh.singletonAsync<_i4.DioRestClient>(
+    gh.singleton<_i3.ConnectivityHelper>(() => _i3.ConnectivityHelper());
+    gh.singleton<_i4.IsolateManager>(() => _i4.IsolateManager());
+    await gh.singletonAsync<_i5.DioRestClient>(
       () {
-        final i = _i4.DioRestClient();
+        final i = _i5.DioRestClient();
         return i.init().then((_) => i);
       },
       preResolve: true,
     );
-    gh.lazySingleton<_i5.IProfileRemote>(
-      () => _i6.ProfileRemoteImpl(gh<_i4.DioRestClient>()),
+    gh.lazySingleton<_i6.IProfileRemote>(
+      () => _i7.ProfileRemoteImpl(gh<_i5.DioRestClient>()),
       instanceName: 'ProfileRemoteImpl',
     );
-    gh.lazySingleton<_i7.IProfileRepository>(
-      () => _i8.ProfileRepositoryImpl(
-          gh<_i5.IProfileRemote>(instanceName: 'ProfileRemoteImpl')),
+    gh.lazySingleton<_i8.IProfileRepository>(
+      () => _i9.ProfileRepositoryImpl(
+          gh<_i6.IProfileRemote>(instanceName: 'ProfileRemoteImpl')),
       instanceName: 'ProfileRepositoryImpl',
     );
-    gh.singleton<_i9.IsolateManager>(_i9.IsolateManager());
     gh.lazySingleton<_i10.LogOutUseCase>(() => _i10.LogOutUseCase(
-        gh<_i7.IProfileRepository>(instanceName: 'ProfileRepositoryImpl')));
+        gh<_i8.IProfileRepository>(instanceName: 'ProfileRepositoryImpl')));
     gh.lazySingleton<_i11.UpdateUserInfoUseCase>(() =>
         _i11.UpdateUserInfoUseCase(
-            gh<_i7.IProfileRepository>(instanceName: 'ProfileRepositoryImpl')));
+            gh<_i8.IProfileRepository>(instanceName: 'ProfileRepositoryImpl')));
     gh.lazySingleton<_i12.DeleteAccountUseCase>(() => _i12.DeleteAccountUseCase(
-        gh<_i7.IProfileRepository>(instanceName: 'ProfileRepositoryImpl')));
+        gh<_i8.IProfileRepository>(instanceName: 'ProfileRepositoryImpl')));
     return this;
   }
 }
